9cfa490d2349ed6f399f0c63c3cf42d26a9ceae1
Cendel Altunisik <cendelaltinisik@gmail.com>
2023-09-03 20:13:29 +0200

    start contactmessages app

---
+++ contactmessages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
