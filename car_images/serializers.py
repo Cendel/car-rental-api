ab3b4e4e805fbe505b85ccb4c6e061549f57e239
Cendel Altunisik <cendelaltinisik@gmail.com>
2023-09-04 13:22:03 +0200

    add UploadFİleView

---
+++ car_images/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Image
+
+base_str = "http://127.0.0.1:8000/media/"  # medya dosyalarının temel URL'si. medya dosyalarının sunucuda nasıl erişileceğini belirtir
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()  # serializer sınıfında yeni bir url alanı oluşturduk. Bu alan, asagidaki get_url adlı özel bir metot tarafından doldurulacak.
+
+    class Meta:  # Meta sınıfı, hangi modelin kullanılacağını ve hangi alanların dönüştürüleceğini belirtir.
+        model = Image
+        fields = ('id', 'name', 'url', 'type', 'size', 'image')  # Dönüştürülen JSON'da hangi alanların yer alacağını belirler. Dökümantasyon'da id belirtilmemisti ama biz ihtiyac olabilecegini düsündük. hoca, aslinda image yerine url'yi kullanabilecegimizi ama her ihtimale karsi imagi i de ekledigini söyledi..
+
+    def get_url(self, obj):  # Bu, url alanını doldurmak için kullanılan özel (custom) bir metottur. Medya dosyasının URL'sini oluşturmak için base_str ile obj.image (resmin adı) birleştirilir.
+        url = base_str + "" + str(obj.image)
+        return url
+
---
+++ car_images/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import Image
+from .serializers import ImageSerializer
+from rest_framework.response import Response
+
+# Create your views here.
+
+
+class UploadFileView(CreateAPIView):
+    parser_classes = (MultiPartParser, FormParser)  # Django REST framework ile dosya yüklemesi yaparken gelen isteği işlemek için kullanılan veri ayrıştırıcıları (parsers) tanımlar. Bu parsers, gelen isteği istemcinin veri formatına göre doğru şekilde ayrıştırmaya yardımcı olur.
+    # MultiPartParser: Bu parser, çoklu parçalı (multipart) form verilerini işlemek için kullanılır. Genellikle dosya yüklemesi için kullanılır. İsteği incelediğinde, birden çok parçadan oluşan veriyi çözümleyerek içeriği ve dosyaları ayırır.
+    # FormParser: Bu parser, normal form verilerini işlemek için kullanılır. Form verileri URL kodlamasına (application/x-www-form-urlencoded) veya JSON verilerine benzer bir şekilde ayrıştırılır.
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = ImageSerializer(data=request.data)  # Gelen istek verilerini ImageSerializer ile işler.
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response({  # dökümantasyonun bizden istedigi sekild Response'u düzenliyoruz.
+                            'message':'Image upload success',
+                            'success': True,
+                            "imageId": str(file_serializer.data['id'])
+                            },status=201)
+        else:
+            return Response(file_serializer.errors, status=400)
+
+
+
+
+
